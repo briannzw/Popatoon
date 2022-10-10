2bfcf2618d6e75245f44be89342a6cd9401a8bd1
Brian Wijaya <70004754+briannzw@users.noreply.github.com>
2022-10-11 00:15:51 +0700

    Folder Restructure

--- Assets/Scripts/Commands/CommandSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Character Command/CommandSO")]
-public class CommandSO : ScriptableObject
-{
-    [SerializeField] public int[] commandNum;
-    [SerializeField] public CharaState state;
-    [SerializeField] public int index;
-}
